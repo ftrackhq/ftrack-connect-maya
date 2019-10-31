17a014b39969e0709b895bb5993aef22d80b68fc
Steve Petterborg <steve.petterborg@ftrack.com>
2019-10-31 09:04:48 -0700

    Use setuptools/pkg_resources for pip version check

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@
 import os
 import re
 import shutil
-import pip
 
-if not pip.__version__.split('.') >= ['19', '3', '0']:
-    raise ValueError('Pip should be version 19.3.0 or higher')
-
+from pkg_resources import parse_version
 from setuptools.command.test import test as TestCommand
 from setuptools import setup, find_packages, Command
+import pip
+
+if parse_version(pip.__version__) < parse_version('19.3.0'):
+    raise ValueError('Pip should be version 19.3.0 or higher')
 
 from pip._internal import main as pip_main
+
 
 # Define paths
 
